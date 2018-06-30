0137b8bd39dce7714148ef75674c923e2da0b79d
VersElectronics <jiachengzhu1994@gmail.com>
2018-06-30 19:44:43 -0400

    added

--- RosBag_Inspector.py
+++ RosBag_Inspector.py
@@ -7,38 +7,27 @@ Display all the topics and msg to give you a quick idea of one
 import rosbag
 import sys
 
-def Rosbag_Inspector(bag, file):
-	
-	topic_list = []
+def Rosbag_Inspector(bag, txt_file):
 
-	for topic, msg, time in bag.read_messages():
+    topic_list = []
 
-    	if topic not in topic_list:
-        	topic_list.append(topic)
+    for topic, msg, time in bag.read_messages():
+        if topic not in topic_list:
+            topic_list.append(topic)
 
     for topic in topic_list:
-    	for topic, msg, time in bag.read_messages(topic):
-
-        	print 'topic =', str(topic)
-        	file.write(str(topic))
-        	print 'msg =', str(msg)
-        	file.write(str(msg))
-
-        break
-
-    file.close()
+        for topic, msg, time in bag.read_messages(topic):
+            txt_file.write(str(topic))
+            txt_file.write(str(msg))
+            break
 
 
 if __name__ == "__main__":
-
-	rosbag_address = sys.argv[1]
-
-	rosbag_name = sys.argv[2]
-	bag = rosbag.Bag(rosbag_name)
-
-	file = open( rosbag_name + '_result.txt','w')
-
-	Rosbag_Inspector(bag, file)
+    rosbag_address = sys.argv[1]
+    rosbag_name = sys.argv[2]
+    bag = rosbag.Bag(rosbag_address)
+    file = open( rosbag_name + '_result.txt','w')
+    Rosbag_Inspector(bag, file)
 
 
 
